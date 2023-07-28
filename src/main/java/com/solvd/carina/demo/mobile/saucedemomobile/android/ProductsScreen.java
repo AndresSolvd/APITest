236db388ff47e2b4a02e9ba60362532ee66bd3a3
andres <agarcianavarro.laba@solvd.com>
2023-07-28 12:11:39 -0400

    fix swipe()

--- src/main/java/com/solvd/carina/demo/mobile/saucedemomobile/android/ProductsScreen.java
+++ src/main/java/com/solvd/carina/demo/mobile/saucedemomobile/android/ProductsScreen.java
@@ -47,7 +47,7 @@ public class ProductsScreen extends ProductsScreenBase {
 
     @Override
     public ProductDetailsScreenBase clickProductTitle(String productTitle) {
-        swipe(productTitleS);
+        swipe(productTitleS.format(productTitle));
         productTitleS.format(productTitle).click();
         return initPage(ProductDetailsScreenBase.class);
     }
